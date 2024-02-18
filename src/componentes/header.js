4b3b4fa102b48d7aebf9f2bb6314dc92492c3bf5
OscarMontilla <montillaacostaoscar@gmail.com>
2024-02-18 22:32:20 +0100

    creacion menus.js para la definicion de roles #16

--- src/componentes/header.js
+++ src/componentes/header.js
@@ -6,16 +6,15 @@ export const header = {
     `
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
   <div class="container">
-  <a class="navbar-brand" href="#/home"
-  ><img
-    src="images/logo.svg"
-    alt=""
-    width="30"
-    height="24"
-    class="d-inline-block align-text-top router-link"
-  />
-  Vanilla Games</a
->
+    <a class="navbar-brand" href="#/home"
+      ><img
+        src="images/logo.svg"
+        alt=""
+        width="30"
+        height="24"
+        class="d-inline-block align-text-top"
+      />
+      Vanilla Games</a
     >
     <button
       class="navbar-toggler"
@@ -29,31 +28,26 @@ export const header = {
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
-  <ul class="navbar-nav mx-auto mb-2 mb-lg-0">
-    <li class="nav-item">
-      <a class="nav-link active router-link" aria-current="page" href="#/home">Home</a>
-    </li>
-    <li class="nav-item">
-      <a class="nav-link router-link" aria-current="page" href="#">TOP5 Proyectos</a>
-    </li>
-    <li class="nav-item">
-      <a class="nav-link router-link" aria-current="page" href="#">A cerca de</a>
-    </li>
-  </ul>
-  <ul class="navbar-nav ms-auto me-2 mb-2 mb-lg-0">
-    <li class="nav-item">
-      <a class="ms-2 btn btn-success router-link" href="#/login">
-        Iniciar sesión
-        <i class="bi bi-box-arrow-in-right"></i>
-      </a>
-    </li>
-    <li class="nav-item">
-      <a class="ms-2 btn btn-outline-light router-link" href="#/registro">
-        Regístrate
-        <i class="bi bi-box-arrow-in-right"></i>
-      </a>
-    </li>
-  </ul>
+      
+      <!-- Menu común para todos los usuarios -->
+      <ul class="navbar-nav mx-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#/home">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" aria-current="page" href="#">TOP5 Proyectos</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" aria-current="page" href="#">A cerca de</a>
+        </li>
+      </ul>
+
+      <!-- Aquí va el Menu rol -->
+      <div id="menuRol"></div>
+    
+      <!-- Aquí va el Menu usuario -->
+      <div id="menuUsuario"></div>
+    
     </div>
   </div>
 </nav>
